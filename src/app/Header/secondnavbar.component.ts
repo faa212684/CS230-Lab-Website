dbfc7e7dd0949dee59692d16148829bb94e5bec9
Tyler <tg0046@mix.wvu.edu>
2022-01-21 14:34:58 -0500

    Started working on Lab1 With adding the first two top nav bars

---
+++ src/app/Header/secondnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'DD-secondnavbar',
+    templateUrl: 'secondnavbar.component.html',
+    styleUrls: ['secondnavbar.component.css']
+})
+export class SecondNavBarComponent{
+
+}
