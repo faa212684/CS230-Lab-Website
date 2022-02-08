cc06a8eb3621ef3d4a08c066f57ef4801495f6e9
Tyler <tg0046@mix.wvu.edu>
2022-02-08 11:55:26 -0500

    added data binding and attributes

--- src/app/Body/menuitems.component.ts
+++ src/app/Body/menuitems.component.ts
@@ -10,17 +10,25 @@ export class MenuItemsComponent{
     header: string;
     statement: string;
 
-    // img2: string;
-    // header2: string;
-    // statement2: string;
+    img2: string;
+    header2: string;
+    statement2: string;
 
-    // img3: string;
-    // header3: string;
-    // statement3: string;
+    img3: string;
+    header3: string;
+    statement3: string;
 
     constructor() {
-        this.img = "";
-        this.header = "";
-        this.statement = "";
+        this.img = "assets/Coffee.jpg";
+        this.header = "SIPPING IS BELIEVING";
+        this.statement = "HANDCRAFTED EXPRESSO DRINKS >";
+
+        this.img2 = "assets/All_Day_Sandwiches_768x768.png";
+        this.header2 = "FRESHLY MADE";
+        this.statement2 = "ALL-DAY BREAKFAST SANDWICHES";
+
+        this.img3 = "assets/Hot Coffee Mobile_640x640.jpg";
+        this.header3 = "DUNKIN' CLASSICS";
+        this.statement3 = "HOT COFFEE";
     }
 }
