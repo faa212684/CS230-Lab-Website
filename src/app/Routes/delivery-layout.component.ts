94cab907ba70015d2ec028696028dd61a2d7ef82
Tyler <tg0046@mix.wvu.edu>
2022-02-21 18:24:27 -0500

    Worked ob Lab 6

--- src/app/Routes/delivery-layout.component.ts
+++ src/app/Routes/delivery-layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'DD-delivery-layout',
-    templateUrl: 'delivery-layout.component.html'
+    templateUrl: 'delivery-layout.component.html',
+    styleUrls: ['delivery-layout.component.css']
 })
 
 export class DeliveryLayoutComponent {
