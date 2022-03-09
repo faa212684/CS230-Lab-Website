ea479496d29c2a81b0ee5df7f02a7235f79a4a7e
Tyler <tg0046@mix.wvu.edu>
2022-03-08 19:44:18 -0500

    firebase stuff

--- src/app/Body/information.service.ts
+++ src/app/Body/information.service.ts
@@ -1,19 +1,21 @@
-import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core";
 import { InformationListModel } from "./information-list.model";
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class InformationService {
-    private baseUrl: string = "https://dunkin-donuts-735b8-default-rtdb.firebaseio.com/"
-    private informationEndpoint: string = "information.json"
+    // private baseUrl: string = "https://dunkin-donuts-735b8-default-rtdb.firebaseio.com/"
+    // private informationEndpoint: string = "information.json"
 
-    constructor(private http:HttpClient) {
+    constructor(private db:AngularFireDatabase) {
 
     }
 
     public getInformation(){
-        return this.http.get<InformationListModel[]>(this.baseUrl + this.informationEndpoint);
+        // return this.http.get<InformationListModel[]>(this.baseUrl + this.informationEndpoint);
+        return this.db.list<InformationListModel>("information").valueChanges();
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'dunkin-donuts-735b8',
+    appId: '1:439852756612:web:948d4aca10e27cd4631cd1',
+    databaseURL: 'https://dunkin-donuts-735b8-default-rtdb.firebaseio.com',
+    storageBucket: 'dunkin-donuts-735b8.appspot.com',
+    apiKey: 'AIzaSyDBFB0yGOmoJ7Btl3UzQ8wzP1uQxz_uRic',
+    authDomain: 'dunkin-donuts-735b8.firebaseapp.com',
+    messagingSenderId: '439852756612',
+    measurementId: 'G-38H4H8HCRK',
+  },
   production: false
 };
 
